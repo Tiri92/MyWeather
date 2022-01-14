d1637ec843f382e76fe7bc0cc95a2b5c4db3baf9
Thierry Bitar <bitarthierry@gmail.com>
2022-01-14 18:56:48 +0100

    Listener on internet connection added and custom snackbar is displayed if connection state change

--- app/src/main/java/thierry/myweather/ui/MainActivity.kt
+++ app/src/main/java/thierry/myweather/ui/MainActivity.kt
@@ -1,12 +1,21 @@
 package thierry.myweather.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
 import thierry.myweather.R
 import thierry.myweather.databinding.ActivityMainBinding
 import thierry.myweather.ui.citiesfragment.CitiesFragment
+import thierry.myweather.utils.Utils
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -15,6 +24,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        connectionListener(binding.root)
 
         binding.bottomNavigation.setOnItemSelectedListener { item ->
             when (item.itemId) {
@@ -40,6 +51,38 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container_view, fragmentInstance)
             .commit()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun connectionListener(rootView: View) {
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        val builder = NetworkRequest.Builder()
+        builder.addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+        builder.addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+
+        val networkRequest = builder.build()
+        connectivityManager.registerNetworkCallback(networkRequest,
+            object : ConnectivityManager.NetworkCallback() {
+                override fun onAvailable(network: Network) {
+                    super.onAvailable(network)
+                    Utils.displayCustomSnackbar(
+                        rootView,
+                        getString(R.string.valid_internet_connection),
+                        ContextCompat.getColor(applicationContext, R.color.green)
+                    )
+                }
+
+                override fun onLost(network: Network) {
+                    super.onLost(network)
+                    Utils.displayCustomSnackbar(
+                        rootView,
+                        getString(R.string.no_internet_connection),
+                        ContextCompat.getColor(applicationContext, R.color.red)
+                    )
+                }
+            })
     }
 
 }
